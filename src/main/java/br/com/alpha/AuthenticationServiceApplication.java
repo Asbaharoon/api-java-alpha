8bda3051c4a3f58f7e6ea649f8c7ad8cb0566465
Anderson <anderson.moraes@m2msolutions.com.br>
2018-11-06 13:54:47 -0200

    Commit Inicial

---
+++ src/main/java/br/com/alpha/AuthenticationServiceApplication.java
@@ -0,0 +1,14 @@
+package br.com.alpha;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class AuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+}
